25197ca412dacb1d7ba40e3c196102e49b76f162
Davo <studio@zurreal.net>
2026-01-03 13:26:04 +0100

    Added Argument Usage for Book Location

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# PYTHON MODULE IMPORTs
+import sys
+
+# OWN MODULE IMPORTs
 from stats import get_num_words
 from stats import get_num_chars
 from stats import sort_chars
 
-frankenstein_file_pth = "books/frankenstein.txt"
+#frankenstein_file_pth = "books/frankenstein.txt"
 
 # OPEN AND RETURN FILE CONTENT
 def get_book_text(file_pth):
@@ -26,14 +30,22 @@ def main():
     #char_count_list = get_book_chars_list(get_book_text("books/frankenstein.txt"))
     #print(char_count_list)
     
+    # CHECK IF ARGUMENT WAS GIVEN
+    if len(sys.argv) < 2:
+        # USAGE EXPLANATION
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    file_pth = sys.argv[1]
+        
     ## REPORT
     print("============ BOOKBOT ============")
-    print("Analyzing book found at %s..." % frankenstein_file_pth)
+    print("Analyzing book found at %s..." % file_pth)
     print("----------- Word Count ----------")
-    print("Found %s total words" % get_num_words(get_book_text(frankenstein_file_pth)))
+    print("Found %s total words" % get_num_words(get_book_text(file_pth)))
     print("--------- Character Count -------")
     # CREATE SORTED LIST OF CHARs
-    chars_sorted_array = sort_chars(get_num_chars(get_book_text(frankenstein_file_pth)))
+    chars_sorted_array = sort_chars(get_num_chars(get_book_text(file_pth)))
     for char_dict in chars_sorted_array:
         print("%s: %s" % (char_dict["name"], char_dict["num"]))
     print("============= END ===============")
